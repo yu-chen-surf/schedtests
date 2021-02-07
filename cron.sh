3a00cccf0d8865f44a767abfa35dda69fc740bb1
Aubrey Li <aubrey.li@linux.intel.com>
2021-02-07 07:19:07 +0000

    cron.sh: Add time and kernel and machine information to the notification

--- cron.sh
+++ cron.sh
@@ -18,10 +18,15 @@ task_notify()
 		echo "Time:" $(date '+%F %T') "\nKernel:" `uname -r` "\nMachine:" `hostname` | \
 			mutt -s "[schedtests]: $task $status" $email_address
 	elif [ $task = "Testing" ]; then
-		./report.py --baseline $run_name | mutt -s "[schedtests]: $task $status" $email_address
+		{
+		echo "Time:" $(date '+%F %T') "\nKernel:" `uname -r` "\nMachine:" `hostname`
+		./report.py --baseline $run_name
+		} | mutt -s "[schedtests]: $task $status" $email_address
 	else
-		./report.py --testname $task --baseline $run_name | \
-			mutt -s "[schedtests]: $task $status" $email_address
+		{
+		echo "Time:" $(date '+%F %T') "\nKernel:" `uname -r` "\nMachine:" `hostname`
+		./report.py --testname $task --baseline $run_name
+		} | mutt -s "[schedtests]: $task $status" $email_address
 	fi
 }
 
