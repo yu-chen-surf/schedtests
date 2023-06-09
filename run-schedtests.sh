64301d0617f0908bf61507974b300b4bc2f832bb
Chen Yu <yu.c.chen@intel.com>
2023-06-09 21:40:38 +0800

    restore the default setting of hackbench
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- run-schedtests.sh
+++ run-schedtests.sh
@@ -20,8 +20,8 @@ iterations=3
 
 start_hackbench()
 {
-	#hackbench_job_list="1 2 4 8"
-	hackbench_job_list="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28"
+	hackbench_job_list="1 2 4 8"
+	#hackbench_job_list="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28"
 	hackbench_iterations=$iterations
 	. $test_path/benchmarks/hackbench.sh
 }
